78127236323e92f251a929ec430751b6bf33b1b1
Katya-Kondratchuk <ekaterina.kondratchuk@gmail.com>
2023-01-16 21:36:48 +0200

    add redirect to registration

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -14,12 +14,36 @@ export const theme = createTheme({
   },
   typography: {
     fontFamily: ['Verdana'].join(','),
-    h6: {
+    h2: {
       fontFamily: 'Verdana',
       fontWeight: 700,
       fontSize: 26,
       color: '#0C121A',
       lineHeight: 1.4,
+      textAlign: 'center',
+      display: 'block',
+      marginBottom: 20,
+    },
+    h3: {
+      fontFamily: 'Verdana',
+      fontStyle: 'normal',
+      fontWeight: 700,
+      fontSize: 48,
+      lineHeight: 1.2,
+      textAlign: 'center',
+      color: '#264061',
+      marginBottom: 44,
+    },
+    subtitle1: {},
+    h4: {
+      fontFamily: 'Verdana',
+      fontStyle: 'normal',
+      fontWeight: 700,
+      fontSize: 14,
+      lineHeight: 1.2,
+      // letter-spacing: 0.04em;
+      color: '#212121',
+      marginBottom: 20,
     },
   },
 });
